259c0289fc92f316ae6dea29e9a402a2d5bad555
Mutali09 <nelsonsimiyu9@gmail.com>
2023-12-19 11:50:48 -0800

    Task 2 edited

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swaps two integers in an array
- * @a: The first integer
- * @b: The second integer
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
 void swap_ints(int *a, int *b)
 {
@@ -15,11 +15,12 @@ void swap_ints(int *a, int *b)
 }
 
 /**
- * selection_sort - Sort an array of integers in an ascending order
- * @array: An array of integers
- * @size: The size of the array
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
- * Description: Prints the array after each swap
+ * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
@@ -28,15 +29,17 @@ void selection_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min = array + i;
 		for (j = i + 1; j < size; j++)
 			min = (array[j] < *min) ? (array + j) : min;
+
 		if ((array + i) != min)
 		{
 			swap_ints(array + i, min);
-			print_awway(array, size);
+			print_array(array, size);
 		}
 	}
 }
